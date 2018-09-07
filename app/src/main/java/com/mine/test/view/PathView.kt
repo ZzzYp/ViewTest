0c9f5109bc50af6ab2fe80499ba00483a21a87ed
zhangyupeng <yupeng.zhang@gometech.com.cn>
2018-09-07 18:05:02 +0800

    代码提交

--- app/src/main/java/com/mine/test/view/PathView.kt
+++ app/src/main/java/com/mine/test/view/PathView.kt
@@ -7,7 +7,6 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
-import android.view.ViewGroup
 
 /**
  * ProjectName:ViewTest
@@ -39,7 +38,26 @@ class PathView(context: Context) : View(context) {
         drawCircle(canvas);
         //user addpath draw line
         drawLines(canvas);
+        //user addpath draw bezier curve line
+        drawBezierCurveLine(canvas);
 
+    }
+
+    private fun drawBezierCurveLine(canvas: Canvas) {
+        var paint: Paint = Paint();
+        paint.color = Color.GREEN;
+        paint.style = Paint.Style.STROKE;
+        paint.strokeWidth = 10F;
+
+        var path: Path = Path();
+
+        path.moveTo(10f, 500f);
+        path.quadTo(100f, 1f, 200f, 500f);
+        canvas.drawPath(path, paint)
+
+        path.moveTo(200f,500f)
+        path.cubicTo(250f,250f,400f,400f,500f,500f)
+        canvas.drawPath(path, paint)
     }
 
     private fun drawLines(canvas: Canvas) {
@@ -49,18 +67,24 @@ class PathView(context: Context) : View(context) {
         paint.strokeWidth = 10F;
 
         var path: Path = Path();
-        path.moveTo(450F,10F);
+        path.moveTo(450F, 10F);
         /**
          * lineTo：绝对坐标
          */
         path.lineTo(450F, 300F);
+        canvas.drawPath(path, paint);
         /**
          * rLineTo:相对坐标
          */
-        path.rLineTo(0F,-150F);
-        path.close();
-        paint.colorFilter
+        paint.color = Color.RED;
+        //重置path，移动到上一根线的终点，开始画下一根线，颜色就可以不同
+        path.reset();
+        path.moveTo(450F, 300F);
+        path.rLineTo(100F, -150F);
         canvas.drawPath(path, paint);
+        path.close();
+
+
     }
 
     private fun drawCircle(canvas: Canvas) {
