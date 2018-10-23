d7e4d1448f900dfb5228e20871dc616a0bf05ff7
ZzzYp <q365840837@hotmail.com>
2018-10-23 15:03:53 +0800

    [feat]:沉浸式状态栏颜色和背景图之间切换mode--ImmersionTestActivity.java

--- app/src/main/java/com/mine/test/ImmersionTestActivity.java
+++ app/src/main/java/com/mine/test/ImmersionTestActivity.java
@@ -19,6 +19,7 @@ import com.mine.test.utils.DpUtils;
 public class ImmersionTestActivity extends AppCompatActivity {
 
     private LinearLayout cl_coordinatorLayout;
+    private RelativeLayout rl_coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +32,7 @@ public class ImmersionTestActivity extends AppCompatActivity {
     private void initview() {
 
         cl_coordinatorLayout = findViewById(R.id.cl_coordinatorLayout);
-//        int statusBarHeight = getStatusBarHeight(this);
-//        int titleBarWidth = DpUtils.dp2Px(this, 42);
-//        RelativeLayout.LayoutParams llp = (RelativeLayout.LayoutParams) cl_coordinatorLayout.getLayoutParams();
-//        llp.height = statusBarHeight + titleBarWidth;
-//        cl_coordinatorLayout.setLayoutParams(llp);
+        rl_coordinatorLayout = findViewById(R.id.rl_coordinatorLayout);
 
 
         findViewById(R.id.bt_color).setOnClickListener(new View.OnClickListener() {
@@ -43,27 +40,42 @@ public class ImmersionTestActivity extends AppCompatActivity {
             public void onClick(View v) {
                 int colorRes = getResources().getColor(R.color.text_uncheck_background_yellow, getTheme());
                 changeColor(colorRes);
-                Log.d("zyp"," cl_coordinatorLayout.getHeight111 : "+cl_coordinatorLayout.getHeight());
             }
         });
         findViewById(R.id.bt_drawable).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Drawable drawable = getResources().getDrawable(R.drawable.picture_memo_romance_tittle_bg, getTheme());
+                Drawable drawable = getResources().getDrawable(R.drawable.gome_picture_memo_autumn_tittle_bg, getTheme());
                 changeDrawable(drawable);
-                Log.d("zyp"," cl_coordinatorLayout.getHeight222 : "+cl_coordinatorLayout.getHeight());
             }
         });
     }
 
     private void changeColor(int colorRes) {
         cl_coordinatorLayout.setBackgroundColor(colorRes);
+        cl_coordinatorLayout.setFitsSystemWindows(true);
+        setTitleBarBgDisAttribute();
     }
+
+
+
     private void changeDrawable(Drawable drawable) {
         cl_coordinatorLayout.setBackground(drawable);
+        cl_coordinatorLayout.setFitsSystemWindows(true);
+        setTitleBarBgDisAttribute();
     }
 
+    private void setTitleBarBgDisAttribute() {
+        int statusBarHeight = getStatusBarHeight(this);
+        int titleBarWidth = DpUtils.dp2Px(this, 42);
+        RelativeLayout.LayoutParams llp = (RelativeLayout.LayoutParams) cl_coordinatorLayout.getLayoutParams();
+        //llp.height = statusBarHeight + titleBarWidth;
+        //llp.topMargin = statusBarHeight;
+        //cl_coordinatorLayout.setLayoutParams(llp);
+        cl_coordinatorLayout.setPadding(0, statusBarHeight, 0, 0);
 
+
+    }
     private void setImmersion() {
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 //            //透明状态栏
